b4dc5478ccedce8569b5e0d9fad1beed81128cad
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2016-12-23 17:08:13 +0100

    slightly faster decoding method

--- src/experimentalavxbase64.c
+++ src/experimentalavxbase64.c
@@ -141,67 +141,64 @@ size_t expavx2_base64_decode(char *out, const char *src, size_t srclen) {
 
         __m256i str = _mm256_loadu_si256((__m256i *)src);
 
-        // inlined function lookup_pshufb from https://github.com/WojciechMula/base64simd/blob/master/decode/lookup.avx2.cpp
-        // description http://0x80.pl/notesen/2016-01-17-sse-base64-decoding.html#vector-lookup-pshufb-update
+        // inlined function lookup_pshufb_bitmask from https://github.com/WojciechMula/base64simd/blob/master/decode/lookup.avx2.cpp
+        // http://0x80.pl/notesen/2016-01-17-sse-base64-decoding.html#lookup-pshufb-bitmask
 
         const __m256i higher_nibble = _mm256_and_si256(_mm256_srli_epi32(str, 4), _mm256_set1_epi8(0x0f));
-        const char linv = 1;
-        const char hinv = 0;
+        const __m256i lower_nibble  = _mm256_and_si256(str, _mm256_set1_epi8(0x0f));
 
-        const __m256i lower_bound_LUT = _mm256_setr_epi8(
-            /* 0 */ linv, /* 1 */ linv, /* 2 */ 0x2b, /* 3 */ 0x30,
-            /* 4 */ 0x41, /* 5 */ 0x50, /* 6 */ 0x61, /* 7 */ 0x70,
-            /* 8 */ linv, /* 9 */ linv, /* a */ linv, /* b */ linv,
-            /* c */ linv, /* d */ linv, /* e */ linv, /* f */ linv,
+        const __m256i shiftLUT = _mm256_setr_epi8(
+            0,   0,  19,   4, -65, -65, -71, -71,
+            0,   0,   0,   0,   0,   0,   0,   0,
 
-            /* 0 */ linv, /* 1 */ linv, /* 2 */ 0x2b, /* 3 */ 0x30,
-            /* 4 */ 0x41, /* 5 */ 0x50, /* 6 */ 0x61, /* 7 */ 0x70,
-            /* 8 */ linv, /* 9 */ linv, /* a */ linv, /* b */ linv,
-            /* c */ linv, /* d */ linv, /* e */ linv, /* f */ linv
+            0,   0,  19,   4, -65, -65, -71, -71,
+            0,   0,   0,   0,   0,   0,   0,   0
         );
 
-        const __m256i upper_bound_LUT = _mm256_setr_epi8(
-            /* 0 */ hinv, /* 1 */ hinv, /* 2 */ 0x2b, /* 3 */ 0x39,
-            /* 4 */ 0x4f, /* 5 */ 0x5a, /* 6 */ 0x6f, /* 7 */ 0x7a,
-            /* 8 */ hinv, /* 9 */ hinv, /* a */ hinv, /* b */ hinv,
-            /* c */ hinv, /* d */ hinv, /* e */ hinv, /* f */ hinv,
+        const __m256i maskLUT  = _mm256_setr_epi8(
+            /* 0        */ (char)(0b10101000),
+            /* 1 .. 9   */ (char)(0b11111000), (char)(0b11111000), (char)(0b11111000), (char)(0b11111000),
+                           (char)(0b11111000), (char)(0b11111000), (char)(0b11111000), (char)(0b11111000),
+                           (char)(0b11111000),
+            /* 10       */ (char)(0b11110000),
+            /* 11       */ (char)(0b01010100),
+            /* 12 .. 14 */ (char)(0b01010000), (char)(0b01010000), (char)(0b01010000),
+            /* 15       */ (char)(0b01010100),
 
-            /* 0 */ hinv, /* 1 */ hinv, /* 2 */ 0x2b, /* 3 */ 0x39,
-            /* 4 */ 0x4f, /* 5 */ 0x5a, /* 6 */ 0x6f, /* 7 */ 0x7a,
-            /* 8 */ hinv, /* 9 */ hinv, /* a */ hinv, /* b */ hinv,
-            /* c */ hinv, /* d */ hinv, /* e */ hinv, /* f */ hinv
+            /* 0        */ (char)(0b10101000),
+            /* 1 .. 9   */ (char)(0b11111000), (char)(0b11111000), (char)(0b11111000), (char)(0b11111000),
+                           (char)(0b11111000), (char)(0b11111000), (char)(0b11111000), (char)(0b11111000),
+                           (char)(0b11111000),
+            /* 10       */ (char)(0b11110000),
+            /* 11       */ (char)(0b01010100),
+            /* 12 .. 14 */ (char)(0b01010000), (char)(0b01010000), (char)(0b01010000),
+            /* 15       */ (char)(0b01010100)
         );
 
-        const __m256i shift_LUT = _mm256_setr_epi8(
-            /* 0 */ 0x00,        /* 1 */ 0x00,        /* 2 */ 0x3e - 0x2b, /* 3 */ 0x34 - 0x30,
-            /* 4 */ 0x00 - 0x41, /* 5 */ 0x0f - 0x50, /* 6 */ 0x1a - 0x61, /* 7 */ 0x29 - 0x70,
-            /* 8 */ 0x00,        /* 9 */ 0x00,        /* a */ 0x00,        /* b */ 0x00,
-            /* c */ 0x00,        /* d */ 0x00,        /* e */ 0x00,        /* f */ 0x00,
+        const __m256i bitposLUT = _mm256_setr_epi8(
+            0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, (char)(0x80),
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 
-            /* 0 */ 0x00,        /* 1 */ 0x00,        /* 2 */ 0x3e - 0x2b, /* 3 */ 0x34 - 0x30,
-            /* 4 */ 0x00 - 0x41, /* 5 */ 0x0f - 0x50, /* 6 */ 0x1a - 0x61, /* 7 */ 0x29 - 0x70,
-            /* 8 */ 0x00,        /* 9 */ 0x00,        /* a */ 0x00,        /* b */ 0x00,
-            /* c */ 0x00,        /* d */ 0x00,        /* e */ 0x00,        /* f */ 0x00
+            0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, (char)(0x80),
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
         );
 
-        const __m256i upper_bound = _mm256_shuffle_epi8(upper_bound_LUT, higher_nibble);
-        const __m256i lower_bound = _mm256_shuffle_epi8(lower_bound_LUT, higher_nibble);
+        const __m256i sh     = _mm256_shuffle_epi8(shiftLUT,  higher_nibble);
+        const __m256i eq_2f  = _mm256_cmpeq_epi8(str, _mm256_set1_epi8(0x2f));
+        const __m256i shift  = _mm256_blendv_epi8(sh, _mm256_set1_epi8(16), eq_2f);
 
-        const __m256i below = _mm256_cmpgt_epi8(lower_bound, str);
-        const __m256i above = _mm256_cmpgt_epi8(str, upper_bound);
-        const __m256i eq_2f = _mm256_cmpeq_epi8(str, _mm256_set1_epi8(0x2f));
+        const __m256i M      = _mm256_shuffle_epi8(maskLUT,   lower_nibble);
+        const __m256i bit    = _mm256_shuffle_epi8(bitposLUT, higher_nibble);
 
-        const __m256i outside = _mm256_andnot_si256(eq_2f, _mm256_or_si256(above, below));
+        const __m256i non_match = _mm256_cmpeq_epi8(_mm256_and_si256(M, bit), _mm256_setzero_si256());
 
-        if (_mm256_movemask_epi8(outside)) {
+        if (_mm256_movemask_epi8(non_match)) {
             break;
         }
         srclen -= 32;
         src += 32;
 
-        const __m256i shift  = _mm256_shuffle_epi8(shift_LUT, higher_nibble);
-        const __m256i t0     = _mm256_add_epi8(str, shift);
-        str = _mm256_add_epi8(t0, _mm256_and_si256(eq_2f, _mm256_set1_epi8(-3)));
+        str = _mm256_add_epi8(str, shift);
 
         // end of inlined function
 
